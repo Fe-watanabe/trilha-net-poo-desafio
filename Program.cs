8be6264503a51c8e908629ef53333470128f2ba4
Felipe Andres Watanabe Pinto <felipe.watanabe.ads@gmail.com>
2023-12-21 13:40:03 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "999774521", modelo: "Nokia C2", imei: "987654321", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+string line = new string('-', 50);
+Console.WriteLine(line);
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "999482316", modelo: "iPhone 15 Pro Max", imei: "654789321", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
