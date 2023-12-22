67fb79dfdb4426c5077e33564b31dae29bae22f6
Felipe Andres Watanabe Pinto <felipe.watanabe.ads@gmail.com>
2023-12-22 11:21:07 -0300

    Desafio Concluído v2

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,16 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        private string Modelo { get; set; }
-        private string Imei { get; set; }
-        private int Memoria { get; set; }
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            Modelo = modelo;
-            Imei = imei;
-            Memoria = memoria;
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
 
         public void Ligar()
